a9f58a755cdd3d874fe647d80e7cdaa21381bf06
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-10-31 16:45:13 +0100

    done for the day

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :flats
+  get 'search', to: 'flats#search', as: :search
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  url: '../storage/ialicante.jpg'
+)
+
+Flat.create!(
+  name: 'Dead and Undead cimetery',
+  address: 'Hell',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  url: '../storage/iankeef.jpg'
+)
+
+Flat.create!(
+  name: 'Dark & Contiguious Cave Flat Shanghai',
+  address: '10, zealand in Harbour',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 95,
+  number_of_guests: 2,
+  url: '../storage/luke.jpg'
+)
+
+Flat.create!(
+  name: 'Gilles',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 1075,
+  number_of_guests: 1,
+  url: '../storage/steph.jpg'
+)
